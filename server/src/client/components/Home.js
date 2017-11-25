2745417f3410f9a78a25993b9a377bf2bf86b90e
Kaloian Ianev <kianev@gmail.com>
2017-11-25 14:38:28 +0200

    server and client side rendering setup using ReactDom.hydrate

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I am home component</div>
-}
+  return (
+    <div>
+      <div>I am home home home component</div>
+      <button onClick={() => {console.log('hi there')}}>Press me!!</button>
+    </div>
+  );
+};
 
 export default Home;
